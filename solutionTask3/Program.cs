2e5766e63c262adbf36ac01959224199d7e5438a
Armen <armen_davidyants@mail.ru>
2022-07-10 18:12:59 +0300

    Максимальное из трех

---
+++ solutionTask3/Program.cs
@@ -0,0 +1,42 @@
+﻿string? firstNumber = Console.ReadLine();
+string? secondNumber = Console.ReadLine();
+string? therdNumber = Console.ReadLine();
+int max;
+max = 0;
+if (firstNumber != null && secondNumber != null && therdNumber != null)
+{ int firstNumber1 = int.Parse(firstNumber);
+  int secondNumber2 = int.Parse(secondNumber);
+  int therdNumber3 = int.Parse(therdNumber); 
+     if (firstNumber1>secondNumber2)
+       {
+         max = firstNumber1;
+           if (firstNumber1>therdNumber3)
+           {
+            max=firstNumber1;
+           }
+            else
+            { 
+             max = therdNumber3;
+            }   
+       }
+      else 
+        {
+          max = secondNumber2;
+           if (secondNumber2>therdNumber3)
+           {
+             max = secondNumber2;
+           }
+           else 
+           {
+             max = therdNumber3;
+           }
+        }   
+        
+
+         
+
+
+}
+Console.WriteLine(max); Console.WriteLine("max");
+
+
