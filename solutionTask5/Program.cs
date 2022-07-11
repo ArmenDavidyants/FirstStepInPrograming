f833b3a1760abd63d6002587a2c2f5cdcf99aed3
Armen <armen_davidyants@mail.ru>
2022-07-11 17:24:48 +0300

    Добавил программу вывода четных чисел

---
+++ solutionTask5/Program.cs
@@ -0,0 +1,29 @@
+﻿string? inputLine = Console.ReadLine();
+string? m= " ";
+
+
+if(inputLine != null)
+{
+  
+  int N = int.Parse(inputLine);
+  int S = 0;
+  while(S <N-1)
+  {
+    
+    S=S+1;
+    if(S% 2==0)
+    {
+     m=m+S+ " ,"; 
+    
+    }
+      
+  }
+  
+  
+   
+  m=m+N;
+  Console.WriteLine(m); 
+
+
+}
+
